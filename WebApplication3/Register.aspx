edabfc8beb72653fd2afe212dea4114a2fa4e0b2
Fergan Yalim <k21006800@kcl.ac.uk>
2022-06-09 18:40:57 +0300

    First Version

---
+++ WebApplication3/Register.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Kaydol<br />
+            <br />
+        </div>
+        Kullanıcı Adı :&nbsp;
+        <asp:TextBox ID="txtKullanıcıAdı" runat="server" Height="16px"></asp:TextBox>
+        <br />
+        <br />
+        Şifre:
+        <asp:TextBox ID="txtŞifre" runat="server" Height="16px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnKayıt" runat="server" OnClick="btnKayıt_Click" Text="Kayıt" />
+        <asp:SqlDataSource ID="Sample" runat="server" ConnectionString="<%$ ConnectionStrings:SQL TutorialConnectionString %>" SelectCommand="SELECT * FROM [Login]"></asp:SqlDataSource>
+        <br />
+        <asp:Label ID="lblSonuç" runat="server" Text=""></asp:Label>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/Register.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnKayıt_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SQL TutorialConnectionString"].ConnectionString))
+            {
+                string query1 = "Insert into [dbo].[Login] (username,password) Values (@username,@password)";
+                string query2 = "Select username from [dbo].[Login] where username = @username";
+
+                SqlCommand insertCommand = new SqlCommand(query1, myConnection);
+                SqlCommand selectCommand = new SqlCommand(query2, myConnection);
+
+                insertCommand.Parameters.AddWithValue("@username", txtKullanıcıAdı.Text);
+                insertCommand.Parameters.AddWithValue("@password", txtŞifre.Text);
+                selectCommand.Parameters.AddWithValue("@username", txtKullanıcıAdı.Text);
+
+                myConnection.Open();
+
+                String result = selectCommand.ExecuteScalar() as String;
+
+                if (String.IsNullOrEmpty(result))
+                {
+                    insertCommand.ExecuteNonQuery();
+                    lblSonuç.Text = "Hesabınız Başarıyla Açıldı";
+                }
+
+                else
+                {
+                    lblSonuç.Text = "Bu Kullanıcı Adı Zaten Alınmış";
+                }
+            }
+
+        }
+    }
+}
